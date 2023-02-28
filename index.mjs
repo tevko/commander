84907673dca7ea183a5bc667c4acfd0cf0dc6f99
tevko <timevko@gmail.com>
2023-02-28 17:32:44 -0600

    process and import node_modules

--- index.mjs
+++ index.mjs
@@ -2,15 +2,23 @@ import fs from 'fs';
 import path from 'path';
 import unzip from 'unzipper';
 
-const Commander = async function Commander({ fetchZipUrl = 'https://DevInfra-dNKuvJhhwog15wXFxbUYtdQRCOMUyBy3P6RPiJt1Nrk0-s.iap.dev0.run/', lambdaContext, lambdaEvent }) {
+const Commander = async function Commander({ fetchZipUrl = 'https://DevInfra-dNKuvJhhwog15wXFxbUYtdQRCOMUyBy3P6RPiJt1Nrk0-s.iap.dev0.run/', lambdaContext, lambdaEvent, fileName = 'lambdaFunc.js' }) {
   const r = await /* global fetch */ fetch(fetchZipUrl);
   const t = await r.blob();
   const arrayBuffer = await t.arrayBuffer();
   const buff = Buffer.from(arrayBuffer);
   const writer = fs.createWriteStream('package.zip');
-  writer.write(buff, () => {
-    fs.createReadStream('package.zip').pipe(unzip.Extract({ path: 'package' }));
+  const writerPromise = new Promise((resolve, reject) => {
+    writer.write(buff, (err) => {
+      if (err) reject(err);
+      const str = fs.createReadStream('package.zip').pipe(unzip.Extract({ path: 'package' }));
+      str.on('close', resolve);
+    });
   });
+  await writerPromise;
+  const {default: f } = await import(`./package/src/${fileName}`);
+  console.log(f())
+  return f({});
 };
 
 Commander({});
